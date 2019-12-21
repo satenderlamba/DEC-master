cd537ef22a6a3b5b63886bcaae47f9e8de96aca4
saksham-kapoor <sakshamkapoor1729@gmail.com>
2019-12-21 13:04:27 +0530

    Integrate ecell website with DEC portal

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-const expressLayouts = require('express-ejs-layouts');
-var logger = require('morgan');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var session = require('express-session');
-const flash = require('connect-flash');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+const expressLayouts = require("express-ejs-layouts");
+var logger = require("morgan");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var session = require("express-session");
+const flash = require("connect-flash");
 var app = express();
 
 // EJS
 app.use(expressLayouts);
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Body-parser
 app.use(express.urlencoded({ extended: true }));
@@ -19,28 +19,28 @@ app.use(express.urlencoded({ extended: true }));
 // Express session
 app.use(
   session({
-    secret: 'secret',
+    secret: "secret",
     resave: true,
     saveUninitialized: true
   })
 );
 
-app.use("/public", express.static(path.join(__dirname, 'public')));
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.use(express.static("static_web"));
 
 // Connect flash
 app.use(flash());
 
 // Global variables
 app.use(function(req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
   next();
 });
 
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 app.use(function(req, res, next) {
   next(createError(404));
@@ -48,10 +48,10 @@ app.use(function(req, res, next) {
 
 app.use(function(err, req, res, next) {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
